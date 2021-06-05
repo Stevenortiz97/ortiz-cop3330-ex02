6692c103030fa70d7f859b7e492c76f475162429
Steven Ortiz <stevenortiz@knights.ucf.edu>
2021-06-04 21:21:25 -0400

    assignment 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Steven Ortiz
  */
+
+//What is the input string? Homer
+//Homer has 5 characters.
+
 public class App 
 {
     public static void main( String[] args )
