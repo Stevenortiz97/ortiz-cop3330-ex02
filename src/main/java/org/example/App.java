3be7a5ee649a1874ed2b5796204b164df69ad566
Steven Ortiz <stevenortiz@knights.ucf.edu>
2021-06-04 22:20:02 -0400

    assignment 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.Scanner;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Steven Ortiz
@@ -8,10 +8,35 @@ package org.example;
 //What is the input string? Homer
 //Homer has 5 characters.
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+
+        String name = readName();
+
+        String outputString = generateOutputString(name);
+
+        printOutput(outputString);
+
+    }
+
+
+    private static void printOutput(String outputString){
+        System.out.println(outputString);
+    }
+
+    private static String readName(){
+        System.out.print("What is the input string?");
+        String name = in.nextLine();
+        return name;
+    }
+
+    private static String generateOutputString(String name) {
+
+        return name + " has " + name.length() + ", characters.";
     }
 }
